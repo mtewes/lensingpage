bca2aa22eeeec05402d4cea81285d130fc3e3735
jooel <s6jooell@uni-bonn.de>
2023-07-02 17:58:10 +0200

    new dm map + random galaxies + flagship

--- euclid_website/scripts/main_scroller_script.js
+++ euclid_website/scripts/main_scroller_script.js
@@ -22,11 +22,14 @@ for(var i=0; i<w; i++){
 var imageDataDst2, imageDataSrc2, imageDataAlphax, imageDataAlphay;
 
 var img_alphax = new Image();
-img_alphax.src = "../images/scrollpage/alphax_gaussian_random_field_1.png"; //path w.r.t. html page
+//img_alphax.src = "../images/scrollpage/alphax_gaussian_random_field_1.png"; //path w.r.t. html page
+img_alphax.src = "../images/scrollpage/alphax_flagship_1.png";
 var img_alphay = new Image();
-img_alphay.src = "../images/scrollpage/alphay_gaussian_random_field_1.png";
+//img_alphay.src = "../images/scrollpage/alphay_gaussian_random_field_1.png";
+img_alphay.src = "../images/scrollpage/alphay_flagship_1.png";
 var img_kappa = new Image();
-img_kappa.src = "../images/scrollpage/kappa_gaussian_random_field_1.png";
+//img_kappa.src = "../images/scrollpage/kappa_gaussian_random_field_1.png";
+img_kappa.src = "../images/scrollpage/kappa_flagship_1.png";
 
 var canvas2 = document.getElementById('wl_kappa_map');
 var dst2 = canvas2.getContext('2d');
@@ -50,13 +53,30 @@ let alpha_y3 = new Array(w);
         alpha_x3[i] = new Array(h);
         alpha_y3[i] = new Array(h);
     }
+let N_gal = document.getElementById('gal_num').max;
+let r3 = new Array(N_gal);
+let x3 = new Array(N_gal);
+let y3 = new Array(N_gal);
+let red3 = new Array(N_gal);
+let green3 = new Array(N_gal);
+for(var i=0; i<N_gal; i++){
+    r3[i] = getRanNum(20,30);
+    x3[i] = getRanNum(-0.9*w/2, 0.9*w/2);
+    y3[i] = getRanNum(-0.9*w/2, 0.9*w/2);
+    red3[i] = getRanNum(200,255);
+    green3[i] = getRanNum(70,130);
+    if(checkOverlap(i,x3,y3,r3) == true){i--;} //prevent galaxy overlap
+}
 
 //structure
 var canvas4 = document.getElementById('structure_map');
 var structure_map = canvas4.getContext('2d');
+var imageDataStr, imageDataStrhelp;
 
 var img_structure = new Image();
 img_structure.src = "../images/scrollpage/Euclid_flagship_mock_galaxy_catalogue_cropped.png";
+var img_euclid = new Image();
+img_euclid.src = "../images/scrollpage/Euclid_telescope.png";
 
 window.onload = function() {
     //first
@@ -74,10 +94,10 @@ window.onload = function() {
 
     let curX;
     let curY;
-    let pressed1 = false;
+    /*let pressed1 = false;
     // update mouse pointer coordinates
     canvas1.addEventListener("mousedown", () => pressed1 = true);
-    canvas1.addEventListener("mouseup", () => pressed1 = false);
+    canvas1.addEventListener("mouseup", () => pressed1 = false);*/
 
     beta_x = -100;
     beta_y = 70;
@@ -94,7 +114,10 @@ window.onload = function() {
         var bounds = canvas1.getBoundingClientRect();
         curX = e.clientX - bounds.left - scrollX - (bounds.right - bounds.left)/2;
         curY = -(e.clientY - bounds.top - scrollY - (bounds.bottom - bounds.top)/2);
-        if(pressed1==true){drawcanvas1(curX, curY, alpha_x1, alpha_y1);}
+        //if(pressed1==true){drawcanvas1(curX, curY, alpha_x1, alpha_y1);}
+        if(Math.abs(curX)<=w && Math.abs(curY)<=h){
+            drawcanvas1(curX, curY, alpha_x1, alpha_y1);
+        }
     });
 
     //second
@@ -120,28 +143,46 @@ window.onload = function() {
             alpha_y2[x][y] = -imageDataAlphay.data[index] + imageDataAlphay.data[index+2]; //canvas y-coord is inverted
         }
     }
-    var alphax_max_norm = 20;
-    var alphay_max_norm = 20;
+    let alpha_max_norm = 25;
     for(var x=0; x<w; x++){
         for(var y=0; y<h; y++){
-            alpha_x2[x][y] *= alphax_max_norm/255;
-            alpha_y2[x][y] *= alphay_max_norm/255;
+            alpha_x2[x][y] *= alpha_max_norm/255;
+            alpha_y2[x][y] *= alpha_max_norm/255;
         }
     }
     
-    let pressed2 = false;
+    /*let pressed2 = false;
     // update mouse pointer coordinates
     canvas2.addEventListener("mousedown", () => pressed2 = true);
-    canvas2.addEventListener("mouseup", () => pressed2 = false);
+    canvas2.addEventListener("mouseup", () => pressed2 = false);*/
 
     drawcanvas2(0, 0, alpha_x2, alpha_y2);
+    var curX2, curY2;
     document.addEventListener("mousemove", (e) => {
         var bounds = canvas2.getBoundingClientRect();
-        curX = e.clientX - bounds.left - scrollX - (bounds.right - bounds.left)/2;
-        curY = -(e.clientY - bounds.top - scrollY - (bounds.bottom - bounds.top)/2);
+        curX2 = e.clientX - bounds.left - scrollX - (bounds.right - bounds.left)/2;
+        curY2 = -(e.clientY - bounds.top - scrollY - (bounds.bottom - bounds.top)/2);
         //if(pressed2==true){drawcanvas2(curX, curY, alpha_x2, alpha_y2);}
-        drawcanvas2(curX, curY, alpha_x2, alpha_y2);
+        if(Math.abs(curX2)<=canvas2.width && Math.abs(curY2)<=canvas2.height){
+            drawcanvas2(curX2, curY2, alpha_x2, alpha_y2);
+        }
     });
+    let pressed2=false;
+    document.getElementById("toggle_dm_wl").onclick = function(){
+        if(pressed2==false){
+            dst2.drawImage(img_kappa, 0, 0);
+            imageDataSrc2 = dst2.getImageData(0,0,w,h);
+            for(var i=0; i<w*h*4; i++){
+                imageDataSrc2.data[i*4]=0; //take out all red
+            }
+            pressed2=true;
+        } else{
+            dst2.fillRect(0,0,w,h);
+            imageDataSrc2 = dst2.getImageData(0,0,w,h);
+            pressed2=false;
+        }
+        drawcanvas2(curX2, curY2, alpha_x2, alpha_y2);
+    };
 
     //third
     lens.drawImage(img_alphax, 0, 0);
@@ -167,28 +208,22 @@ window.onload = function() {
             alpha_y3[x][y] = -imageDataAlphay.data[index] + imageDataAlphay.data[index+2]; //canvas y-coord is inverted
         }
     }
-    var alpha_max_norm3 = document.getElementById('alpha_scale').value;
-    var alpha_max_norm_help = alpha_max_norm3;
-    document.getElementById('alpha_scale_out').value = alpha_max_norm3;
     for(var x=0; x<w; x++){
         for(var y=0; y<h; y++){
-            alpha_x3[x][y] *= alpha_max_norm3/255;
-            alpha_y3[x][y] *= alpha_max_norm3/255;
+            alpha_x3[x][y] *= alpha_max_norm/255;
+            alpha_y3[x][y] *= alpha_max_norm/255;
         }
     }
-    d_gal = w/10
-    document.getElementById("alpha_scale").onclick = function(){
-        alpha_max_norm3 = document.getElementById('alpha_scale').value;
-        document.getElementById('alpha_scale_out').value = alpha_max_norm3;
-        for(var x=0; x<w; x++){
-            for(var y=0; y<h; y++){
-                alpha_x3[x][y] *= alpha_max_norm3/alpha_max_norm_help;
-                alpha_y3[x][y] *= alpha_max_norm3/alpha_max_norm_help;
-            }
-        }
-        alpha_max_norm_help = alpha_max_norm3;
-        drawcanvas3(alpha_x3, alpha_y3, d_gal);
-    };
+
+    //let d_gal = w/10;
+    let gal_num_range = document.getElementById('gal_num');
+    var N_gal_used = gal_num_range.value;
+    document.getElementById('gal_num_out').value = N_gal_used;
+    gal_num_range.addEventListener('mouseup', function(){
+        N_gal_used = gal_num_range.value;
+        document.getElementById('gal_num_out').value = N_gal_used;
+        drawcanvas3(alpha_x3,alpha_y3,N_gal_used);
+    })
     let pressed3=false;
     document.getElementById("toggle_dm_gal_grid").onclick = function(){
         if(pressed3==false){
@@ -203,13 +238,40 @@ window.onload = function() {
             black = lens.getImageData(0,0,w,h);
             pressed3=false;
         }
-        drawcanvas3(alpha_x3, alpha_y3, d_gal);
+        drawcanvas3(alpha_x3, alpha_y3, N_gal_used);
     };
     
-    drawcanvas3(alpha_x3, alpha_y3, d_gal);
+    drawcanvas3(alpha_x3, alpha_y3, N_gal_used);
 
     //structure_map
-    var age;
+    //structure_map.fillStyle = 'black';
+    //structure_map.fillRect(0,0,canvas4.width,canvas4.height);
+    let w_stripe = 200;
+    let euclid_offset = 0.4*w_stripe*img_euclid.height/img_euclid.width;
+    structure_map.drawImage(img_euclid, 0, 0, img_euclid.width, img_euclid.height, 0, (w_stripe-euclid_offset*img_euclid.height/img_euclid.width)/2, euclid_offset, euclid_offset*img_euclid.height/img_euclid.width)
+    let w_image = Math.round(canvas4.width/2);
+    let h_image = canvas4.height - (w_stripe+50);
+    let w_clip = h_image*(canvas4.width-euclid_offset)/w_stripe;
+    let w_clip_stripe = w_clip*(canvas4.width-euclid_offset)/img_structure.width;
+    structure_map.drawImage(img_structure, 0, 0, img_structure.width, img_structure.height, euclid_offset, 0, canvas4.width, w_stripe);
+    imageDataStr = structure_map.getImageData(0,0,canvas4.width,canvas4.height);
+    imageDataStrhelp = structure_map.getImageData(0,0,canvas4.width,canvas4.height);
+    var curXs = w_clip_stripe/2 + euclid_offset
+    var curYs = 0;
+    var curXsold = curXs;
+    drawcursor(curXs, w_stripe, curXsold);
+    structure_map.drawImage(img_structure, curXs/canvas4.width*img_structure.width - w_clip/2, 0, w_clip, img_structure.height, (canvas4.width-w_image)/2, w_stripe+50, w_image, h_image);
+    document.addEventListener("mousemove", (e) => {
+        var bounds = canvas4.getBoundingClientRect();
+        curXs = e.clientX - bounds.left - scrollX;
+        curYs = -(e.clientY - bounds.top - scrollY);
+        if(curXs <= canvas4.width - w_clip_stripe/2 && curXs >= w_clip_stripe/2 + euclid_offset && curYs <= 0 && curYs >= -w_stripe){
+            drawcursor(curXs, w_stripe, curXsold);
+            curXsold = curXs;
+            structure_map.drawImage(img_structure, (curXs-euclid_offset)/(canvas4.width-euclid_offset)*img_structure.width - w_clip/2, 0, w_clip, img_structure.height, (canvas4.width-w_image)/2, w_stripe+50, w_image, h_image);
+        }
+    });
+    /*var age;
     let max_age = 13.76;
     let min_age = 2.86;
     min_age += canvas4.width / img_structure.width * (max_age - min_age); //age at left border of canvas
@@ -223,7 +285,7 @@ window.onload = function() {
         document.getElementById('universe_age_out').value = age.toFixed(1) + ' Mrd. Jahre';
         xOffset = (img_structure.width - canvas4.width) * range.value / range.max;
         structure_map.drawImage(img_structure, xOffset, yOffset, canvas4.width, canvas4.height, 0, 0, canvas4.width, canvas4.width);
-      })
+      })*/
 };
 
 function drawlens1(or, q, a, thetaE, thetaC){
@@ -295,9 +357,9 @@ function drawcanvas2(beta_x, beta_y, alpha_x2, alpha_y2) {
             d = Math.sqrt(Math.pow(beta_x - src_x, 2) + Math.pow(beta_y - src_y, 2));
             if(d <= r){
                 index = (x + y*w)*4;
-                imageDataDst2.data[index] = imageData_profile(d, r/2.8, 255);
-                imageDataDst2.data[index+1] = imageData_profile(d, r/2.8, 100);
-                imageDataDst2.data[index+2] = imageData_profile(d, r/2.8, 100);
+                imageDataDst2.data[index] += imageData_profile(d, r/2.8, 255);
+                imageDataDst2.data[index+1] += imageData_profile(d, r/2.8, 100);
+                imageDataDst2.data[index+2] += imageData_profile(d, r/2.8, 100);
                 imageDataDst2.data[index+3] = 255;
             }
         }
@@ -306,7 +368,7 @@ function drawcanvas2(beta_x, beta_y, alpha_x2, alpha_y2) {
     dst2.putImageData(imageDataDst2, 0, 0);
 }
 
-function drawcanvas3(alpha_x3, alpha_y3, d_gal) {
+function drawcanvas3(alpha_x3, alpha_y3, N_gal_used) {
     for(var i=0; i<h*w*4; i++){
         imageDataLens.data[i] = black.data[i];
     }
@@ -316,7 +378,17 @@ function drawcanvas3(alpha_x3, alpha_y3, d_gal) {
         for(var x=0; x<w; x++){
             src_x = (x-origin_x) - alpha_x3[x][y]; //arbitrary deflection angle from array alpha_x,y
             src_y = (origin_y-y) - alpha_y3[x][y];
-            src_x = origin_x + src_x;
+            for(var i=0; i<N_gal_used; i++){
+                d = Math.sqrt(Math.pow(x3[i]-src_x, 2) + Math.pow(y3[i]-src_y, 2));
+                if(d<=r3[i]){
+                    index = (x + y*w)*4;
+                    imageDataLens.data[index] += imageData_profile(d, r3[i]/2.8, red3[i]);
+                    imageDataLens.data[index+1] += imageData_profile(d, r3[i]/2.8, green3[i]);
+                    imageDataLens.data[index+2] += imageData_profile(d, r3[i]/2.8, 100);
+                    imageDataLens.data[index+3] = 255;
+                }
+            }
+            /*src_x = origin_x + src_x;
             src_y = origin_y - src_y;
             for(var px=d_gal; px<w-r; px+=d_gal){
                 for(var py=d_gal; py<h-r; py+=d_gal){
@@ -329,16 +401,6 @@ function drawcanvas3(alpha_x3, alpha_y3, d_gal) {
                         imageDataLens.data[index+3] = 255;
                     }
                 }
-            }
-            /*tol = 0.5;
-            for(var i=d_gal; i<w; i+=d_gal){ //draw grid
-                if(Math.round(src_x) <= Math.round(i)+tol && Math.round(src_x) >= Math.round(i)-tol){
-                    index = (x + y*w)*4;
-                    imageDataLens.data[index+1] = 200;
-                } else if(Math.round(src_y) <= Math.round(i)+tol && Math.round(src_y) >= Math.round(i)-tol){
-                    index = (x + y*w)*4;
-                    imageDataLens.data[index+1] = 200;
-                }
             }*/
         }
     }
@@ -346,6 +408,38 @@ function drawcanvas3(alpha_x3, alpha_y3, d_gal) {
     lens.putImageData(imageDataLens, 0, 0);
 }
 
+function drawcursor(curXs, w_stripe, curXsold){
+    for(var i=0; i<w_stripe; i++){
+        for(var j=-2; j<=2; j++){
+            index = Math.round((curXs + (i+j)*canvas4.width)*4);
+            indexold = Math.round((curXsold + (i+j)*canvas4.width)*4);
+            for(var k=0; k<3; k++){
+                imageDataStr.data[indexold+k] = imageDataStrhelp.data[indexold+k];
+                imageDataStr.data[index+k] = 255;
+            }
+            imageDataStr.data[indexold+3] = imageDataStrhelp.data[indexold+3];
+            imageDataStr.data[index+3] = 255;
+        }
+    }
+    structure_map.putImageData(imageDataStr, 0, 0);
+}
+
 function imageData_profile(d, r_scale, max){
     return max*Math.exp(-3.6713 * (Math.pow(d/r_scale, 1/2) - 1)); //Sersic profile with n=2
+}
+
+function getRanNum(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function checkOverlap(index,x,y,r){
+    for(var j=0; j<index; j++){
+        d_x = x[index] - x[j];
+        d_y = y[index] - y[j];
+        d = Math.sqrt(Math.pow(d_x, 2) + Math.pow(d_y, 2));
+        if(d < 1.5*(r[index] + r[j])){ //factor 2 tries to account for stretching in lens plane
+            return true;
+        }
+    }
+    return false;
 }
